dae1fbd80d6e2e0479749043e37844a45dbd24b3
phoenixgu <phoenixgf0603@gmail.com>
2022-04-30 13:53:17 -0400

    graph updated

--- src/ca/bytetube/_10_graph/Graph.java
+++ src/ca/bytetube/_10_graph/Graph.java
@@ -1,19 +1,20 @@
 package ca.bytetube._10_graph;
 
-import ca.bytetube._07_AVLTree.BinaryTree;
-
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public abstract class Graph<V,E> {
 
+    protected WeightManager<E> weightManager;//增加成员属性
 
-    protected WeightManager<E> weightManager;
-    public Graph(WeightManager<E> weightManager) {
+    public Graph() {}//无参
+
+    public Graph(WeightManager<E> weightManager) {//单参
         this.weightManager = weightManager;
     }
 
-    public Graph() {}
 
     public abstract int verticesSize();
 
@@ -37,26 +38,59 @@ public abstract class Graph<V,E> {
 
     public abstract void dfs(V begin,VertexVisitor<V> visitor);
 
-    public abstract Map<V, E> shortestPath(V begin);
-
-    public interface WeightManager<E> {
-        public int compare(E w1, E w2);
-        public E add(E w1, E w2);
-        public E zero();
-    }
-
     public abstract Set<EdgeInfo<V,E>> mst();
 
+    public abstract Map<V, E> shortestPath(V begin);
+
+    public abstract Map<V, PathInfo<V,E>> shortestPath02(V begin);
+
+    public abstract Map<V, Map<V, PathInfo<V,E>>> shortestPath();//Floyd
 
 
+    public interface VertexVisitor<V>{
+        public boolean visit(V v);
+    }
 
 
+    public static class PathInfo<V,E> {
+        protected E weight;
+        protected List<EdgeInfo<V,E>> edgeInfos = new LinkedList<>();
+
+        public PathInfo() { }
+
+        public PathInfo(E weight) {
+            this.weight = weight;
+        }
+
+        public E getWeight() {
+            return weight;
+        }
+
+        public void setWeight(E weight) {
+            this.weight = weight;
+        }
+
+        public List<EdgeInfo<V, E>> getEdgeInfos() {
+            return edgeInfos;
+        }
+
+        public void setEdgeInfos(List<EdgeInfo<V, E>> edgeInfos) {
+            this.edgeInfos = edgeInfos;
+        }
+
+        @Override
+        public String toString() {
+            return "pathInfo{" +
+                    "weight=" + weight +
+                    ", edgeInfos=" + edgeInfos +
+                    '}';
+        }
+    }
 
     public static class EdgeInfo<V,E> {
         private V from;
         private V to;
         private E weight;
-
 
         public EdgeInfo(V from, V to, E weight) {
             this.from = from;
@@ -91,16 +125,20 @@ public abstract class Graph<V,E> {
         @Override
         public String toString() {
             return "EdgeInfo{" +
-                    "weight=" + weight +
-                    ", from=" + from +
+                    "from=" + from +
                     ", to=" + to +
+                    ", weight=" + weight +
                     '}';
         }
     }
 
 
-
-    public interface VertexVisitor<V>{
-        public boolean visit(V v);
+    //管理权重. 设计成接口. 不同类型有不同比较规则
+    public interface WeightManager<E> {
+        public int compare(E w1, E w2);
+        public E add(E w1, E w2);
+        public E initial();
+        //...增加需要的方法
     }
+
 }
