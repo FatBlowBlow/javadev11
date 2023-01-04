ec2290e1f81d7a58ccc6aa861c5aac267cc37a32
phoenixgu <phoenixgf0603@gmail.com>
2023-01-03 23:10:49 -0500

    revise GetMax

--- src/ca/bytetube/day06/GetMax.java
+++ src/ca/bytetube/day06/GetMax.java
@@ -18,7 +18,7 @@ public class GetMax {
             throw new Exception("array cannot be null and length cannot be 0");
         }
         int max = arr[0];
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
             }
@@ -31,7 +31,7 @@ public class GetMax {
             throw new Exception("array cannot be null and length cannot be 0");
         }
         int max = arr[0];
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
           max = max > arr[i]? max : arr[i];
         }
         return max;
